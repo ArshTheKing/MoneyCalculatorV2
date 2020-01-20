40ee4c804b768920a7be89122a7b2623f5dcd4d0
AZAEL <AZAEL@DESKTOP-Q8MDA1A>
2020-01-20 17:09:50 +0000

    CIPO base

---
+++ src/moneycalculatorv2/MoneyCalculatorV2.java
@@ -0,0 +1,29 @@
+package moneycalculatorv2;
+
+public class MoneyCalculatorV2 {
+
+    public static void main(String[] args) {
+        MoneyCalculatorV2 moneyCalculatorV2 = new MoneyCalculatorV2();
+        moneyCalculatorV2.exec();
+        
+    }
+
+    private void exec() {
+        input();
+        process();
+        output();
+    }
+
+    private void input() {
+        
+    }
+
+    private void process() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void output() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
